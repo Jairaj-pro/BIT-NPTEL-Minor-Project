1335b3b637791940c7e104bac48e10509ec4f52a
JairajSinghProd <jairaj2002@gmail.com>
2021-12-11 21:05:40 +0530

    Unlinked barcharts and piecharts script file

--- charts/barchart.js
+++ charts/barchart.js
@@ -1,23 +1,24 @@
-google.charts.load('current', { 'packages': ['bar'] });
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', { 'packages': ['bar'] });
+// google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    //BAR CHART
-    var data = google.visualization.arrayToDataTable([
-        ['Year', 'Enrollments', 'Registrations', 'Succesful Completion'],
-        ['July-2019', 1000, 400, 200],
-        ['Jan-2019', 1170, 460, 250],
-        ['July-2018', 1120, 660, 300],
-        ['Jan-2018', 1030, 540, 350]
-    ]);
 
-    var options = {
-        chart: {
-            title: 'Performance Comparison',
-            subtitle: 'Enrollments, Registrations, and Succesful Completion: 2019-2018',
-        }
-    };
+// function drawChart() {
+//     //BAR CHART
+//     var data = google.visualization.arrayToDataTable([
+//         ['Year', 'Enrollments', 'Registrations', 'Succesful Completion'],
+//         ['July-2019', 1000, 400, 200],
+//         ['Jan-2019', 1170, 460, 250],
+//         ['July-2018', 1120, 660, 300],
+//         ['Jan-2018', 1030, 540, 350]
+//     ]);
 
-    var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-    chart.draw(data, google.charts.Bar.convertOptions(options));
-}
+//     var options = {
+//         chart: {
+//             title: 'Performance Comparison',
+//             subtitle: 'Enrollments, Registrations, and Succesful Completion: 2019-2018',
+//         }
+//     };
+
+//     var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+//     chart.draw(data, google.charts.Bar.convertOptions(options));
+// }
--- charts/piechart.js
+++ charts/piechart.js
@@ -1,22 +1,22 @@
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load("current", { packages: ["corechart"] });
+// google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    //PIE CHART
-    var data = google.visualization.arrayToDataTable([
-        ['Department', 'Succesfull Completion'],
-        ['CSE', 11],
-        ['IT', 2],
-        ['ETC', 2],
-        ['EEE', 2],
-        ['Mechanical', 7]
-    ]);
+// function drawChart() {
+//     //PIE CHART
+//     var data = google.visualization.arrayToDataTable([
+//         ['Department', 'Succesfull Completion'],
+//         ['CSE', 11],
+//         ['IT', 2],
+//         ['ETC', 2],
+//         ['EEE', 2],
+//         ['Mechanical', 7]
+//     ]);
 
-    var options = {
-        title: 'Department Summary',
-        pieHole: 0.4,
-    };
+//     var options = {
+//         title: 'Department Wise Certification Statistics',
+//         pieHole: 0.4,
+//     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-    chart.draw(data, options);
-}
+//     var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+//     chart.draw(data, options);
+// }
