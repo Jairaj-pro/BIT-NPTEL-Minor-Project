55d75ba309a8c46ca4f3341857d0d0dcda615f1c
Jairaj-pro <56227519+Jairaj-pro@users.noreply.github.com>
2021-12-22 17:31:05 +0530

    Delete JAN SEASON 2019/JAN SEASON 2019 directory

--- JAN SEASON 2019/JAN SEASON 2019/BITD ENROLLMENTS 08-02-2019/courses offered at LC.sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-
---total courses offered at LC from BITD ENROLL EVEN 2019 FINAL
-SELECT count(distinct([Course_Id]))
-  FROM [new database].[dbo].[BITD ENROLL EVEN 2019 FINAL]
--- JAN SEASON 2019/JAN SEASON 2019/BITD ENROLLMENTS 08-02-2019/total mentors at LC.sql
+++
@@ -1,2 +0,0 @@
---total mentors at LC
-SELECT count(distinct([Mentor_Name])) FROM [new database].[dbo].[SNPTEL JAN-JUN 2019 MENTORS FINAL]
